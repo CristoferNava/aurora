d3523801c49769d6319ee2e37ec42ee92cd95a63
CristoferNava <cj.carmonanava@ugto.mx>
2020-12-08 11:35:58 -0600

    traversing using for

--- pointers.cpp
+++ pointers.cpp
@@ -6,6 +6,6 @@ using namespace std;
 int main() {
     vector<int> v = {1, 2, 3, 4, 5, 6};
 
-    for (auto &x : v)
-        cout << x << " " << endl;
+    for (int i = 0; i < v.size(); ++i) 
+        cout << v[i] << " ";
 }
